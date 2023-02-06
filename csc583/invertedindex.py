d1cd573eb93427f4c0f422690e144fab050514db
Urvika-gola <urvika.gola@gmail.com>
2023-02-05 19:54:16 -0700

    Removed import for Dict typehinting, changed typehinting to fix build issue
    Signed-off-by: Urvika-gola <urvika.gola@gmail.com>

--- csc583/invertedindex.py
+++ csc583/invertedindex.py
@@ -1,5 +1,4 @@
 import re
-from typing import Dict
 
 
 class InvertedIndex:
@@ -124,7 +123,7 @@ class InvertedIndex:
             del split_query[operator_idx - 1: operator_idx + 2]
         return ["Doc" + str(value) for value in store_intermediate_results[split_query[0]]]
 
-    def _create_inverted_index(self, lines: list[str]) -> Dict[str, list]:
+    def _create_inverted_index(self, lines: list) -> dict:
         """
         Private method to create the inverted index
         :param lines: the lines of the Docs.txt as a String Array
